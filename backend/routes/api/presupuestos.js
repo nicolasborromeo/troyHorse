cd17fd7c883cc5cd250a1b2cabb3ccec7ab35237
Nicolas Borromeo <nicoborrom@gmail.com>
2024-06-13 23:46:29 -0700

    full parsing of productpresupuestos post body

--- backend/routes/api/presupuestos.js
+++ backend/routes/api/presupuestos.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const { Product, Presupuesto, ProductsPresupuesto, sequelize } = require('../../db/models')
+const { check, body } = require('express-validator')
+const { handleValidationError, handleValidationErrors } = require('../../utils/validation')
 
 //middleware
-const checkDuplicate = async(req, res, next) => {
-    const {codigoPresupuesto} = req.body
+const checkDuplicate = async (req, res, next) => {
+    const { codigoPresupuesto } = req.body
     let presuInDatabase = await Presupuesto.findOne({
-        where: {codigo : codigoPresupuesto}
+        where: { codigo: codigoPresupuesto }
     })
 
-    if(presuInDatabase) {
+    if (presuInDatabase) {
         let err = new Error('Invoice already exists in database');
         err.status = 500
         err.message = 'Invoice already exists in database'
@@ -35,7 +37,19 @@ router.get('/', async (req, res, next) => {
     res.status(200).json(presupuestos)
 })
 
-
+const validateBody = [
+    check('cantidad')
+        .exists()
+        .notEmpty().withMessage('Por favor ingrese una cantidad')
+        .isNumeric()
+        .custom = (val) => {
+            if (val <= 0) {
+                let err = new Error('Cantidad tiene que ser mayor que 0')
+            }
+            return true
+        },
+    handleValidationErrors
+]
 
 router.post('/', checkDuplicate, async (req, res, next) => {
     //GET DATA FROM BODY
@@ -72,12 +86,13 @@ router.post('/', checkDuplicate, async (req, res, next) => {
 
     // GET PRODUCTOS [{},{}] and Iterate
     const { productos } = req.body
+
+
     productos.forEach(async reqProduct => {
 
-        const descuento = reqProduct['descuento'] === '' ? 0 : parseInt(reqProduct['descuento']);
 
 
-        const productInDb = await Product.findOne({
+        let productInDb = await Product.findOne({
             where: { codigo: reqProduct.codigo },
             attributes: ['id', 'codigo', 'descripcion']
         })
@@ -85,46 +100,54 @@ router.post('/', checkDuplicate, async (req, res, next) => {
         productInDb ? prodAttributes = productInDb.dataValues : undefined;
 
         //If the reqProduct  doens't exist assing codigo and Id to null
-        if (!productInDb) {
-            reqProduct['codigo'] = null
-            reqProduct['id'] = null
+        // if (!productInDb) {
+        //     reqProduct['codigo'] = null
+        //     reqProduct['id'] = null
+        //     //otherwise check that the values match
+        // } else {
+            // if (productInDb && !reqProduct.codigo === prodAttributes.codigo ||
+            //     !reqProduct.descripcion === prodAttributes.descripcion) {
+            //     let err = new Error('Not Matching');
+            //     err.status = 500
+            //     err.message = "Values of product not matching in the database"
+            //     err.errors = [{ requestValues: "codigo o descripcion distintas al producto en la base de datos" }]
+            //     return next(err)
+            // } else {
+            //     reqProduct['id'] = prodAttributes.id
+            //     reqProduct['codigo'] = prodAttributes.codigo
+            //     reqProduct['descripcion'] = prodAttributes.descripcion
+            // }
 
-            //otherwise check that the values match
-        } else {
-
-            if (!reqProduct.codigo === prodAttributes.codigo ||
-                !reqProduct.descripcion === prodAttributes.descripcion) {
-                let err = new Error('Not Matching');
-                err.status = 500
-                err.message = "Values of product not matching in the database"
-                err.errors = [{ requestValues: "codigo o descripcion distintas al producto en la base de datos" }]
-                return next(err)
-            }
-            reqProduct['id'] = prodAttributes.id
-            reqProduct['codigo'] = prodAttributes.codigo
-            reqProduct['descripcion'] = prodAttributes.descripcion
-        }
+        // }
+        let parsedDescuento = reqProduct['descuento'] === '' ? 0 : parseInt(reqProduct['descuento']);
+        let parsedCantidad = reqProduct['cantidad'] ? Number(reqProduct['cantidad']) : 1
+        let parsedTotal = reqProduct['p-total'] ? parseFloat(reqProduct['p-total']) : 0
+        let parsedPrecio = reqProduct['p-unitario'] ? parseFloat(reqProduct['p-unitario']) : 0
+        let parsedCodigo = reqProduct['codigo'] ? Number(reqProduct['codigo']) : null
+        let parsedProdId = productInDb ? Number(productInDb.dataValues.id) :  null
         //this console loge PROVES that this if else statement is passing (when passing a null product and one in the database)
         // console.log(productInDb, '============================')
         // console.log(reqProduct)
-
-    try {
-        await ProductsPresupuesto.create({
-            productId: parseInt(reqProduct['id']),
-            presupuestoId: parseInt(presupuestoId),
-            codigo: reqProduct['codigo'],
+        // let productId =
+        let productPack = {
+            productId: parsedProdId,
+            presupuestoId: presupuestoId,
+            codigo: parsedCodigo,
             descripcion: reqProduct['descripcion'],
-            cantidad: parseInt(reqProduct['cantidad']),
-            precioUnit: parseFloat(reqProduct['p-unitario']),
-            descuento: descuento,
-            precioTotal: parseFloat(reqProduct['p-total'])
-        })
+            cantidad: parsedCantidad,
+            precioUnit: parsedPrecio,
+            descuento: parsedDescuento,
+            precioTotal: parsedTotal
+        }
+        // console.log(productPack)
+        try {
+            await ProductsPresupuesto.create(productPack)
 
-    } catch (error) {
-        console.error(error.status, error.message)
-        next(error)
-        // res.status(error.status).json(error.message)
-    }
+        } catch (error) {
+            console.error(error.status, error.message)
+            next(error)
+            // res.status(error.status).json(error.message)
+        }
         //create each ProductsPresupuesto
 
     })
